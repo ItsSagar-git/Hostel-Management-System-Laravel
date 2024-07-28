d0d82f23b64a580cc8e44bc63071c12d90a710db
Sagar Adhikari <sagar.adhikary675@gmail.com>
2024-07-28 10:45:43 +0545

    Delete HMS directory

--- HMS/database/migrations/2024_07_20_045737_create_rooms_table.php
+++
@@ -1,9 +0,0 @@
-<!-- use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
- 
-Schema::create('users', function (Blueprint $table) {
-    $table->id();
-    $table->string('name');
-    $table->string('email');
-    $table->timestamps();
-}); -->
--- HMS/resources/js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('searchForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Search function is currently under development.');
-});
-
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Message sent successfully!');
-});
